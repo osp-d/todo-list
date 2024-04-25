850441b4070766f3aa2770d7eae5fd16ef0afe98
osp-d <edudamirosp@gmail.com>
2024-04-25 21:58:16 +0500

    Initial commit

---
+++ todos.js
@@ -0,0 +1,14 @@
+class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+class Project {
+  constructor(title) {
+    this.title = title;
+  }
+}
