944a72a2801ea1407b87cde274d66b53aa3fd31d
osp-d <edudamirosp@gmail.com>
2024-04-26 15:47:30 +0500

    Add project manipulation methods

--- src/todos.js
+++ src/todos.js
@@ -1,3 +1,5 @@
+let projectsArray = [];
+
 class Project {
   constructor(title) {
     this.title = title;
@@ -11,6 +13,43 @@ class Project {
       }
     });
   }
+
+  showTodo(name) {
+    this.storage.forEach((item) => {
+      if (item.title == name) {
+        console.log(item);
+      }
+    });
+  }
+
+  changeTodoStatus(name) {
+    this.storage.forEach((item) => {
+      if (item.title == name) {
+        item.changeStatus();
+      }
+    });
+  }
+
+  editTodo(
+    name,
+    newTitle,
+    newDescription,
+    newDueDate,
+    newPriority,
+    newProject
+  ) {
+    this.storage.forEach((item) => {
+      if (item.title == name) {
+        item.edit(
+          newTitle,
+          newDescription,
+          newDueDate,
+          newPriority,
+          newProject
+        );
+      }
+    });
+  }
 }
 
 class Todo {
@@ -20,14 +59,10 @@ class Todo {
     this.dueDate = dueDate;
     this.priority = priority;
     this.project = project;
+    this.status = 'Incomplete';
   }
 
-  addTodo() {
-    this.project.storage.push(this);
-  }
-  s;
-
-  editTodo(
+  edit(
     newTitle = this.title,
     newDescription = this.description,
     newDueDate = this.dueDate,
@@ -40,8 +75,57 @@ class Todo {
     this.priority = newPriority;
     this.project = newProject;
   }
+
+  changeStatus() {
+    if (this.status === 'Complete') {
+      this.status = 'Incomplete';
+    } else if (this.status === 'Incomplete') {
+      this.status = 'Complete';
+    }
+  }
+}
+
+function addProject(name) {
+  if (projectsArray.length == 0) {
+    projectsArray.push(new Project(name));
+  } else {
+    projectsArray.forEach((item) => {
+      if (item.title == name) {
+        console.log('Such project already exists');
+      } else {
+        projectsArray.push(new Project(name));
+      }
+    });
+  }
+}
+
+function renameProject(name, newName) {
+  projectsArray.forEach((item) => {
+    if (item.title == name) {
+      projectsArray.forEach((item) => {
+        if (item.title == newName) {
+          console.log('Such project already exists');
+        } else {
+          item.name = newName;
+        }
+      });
+    }
+  });
 }
 
 function deleteProject(name) {
-  name = null;
+  projectsArray.forEach((item) => {
+    if (item.title == name) {
+      projectsArray.splice([projectsArray.indexOf(item)], 1);
+    }
+  });
+}
+
+function addTodo(title, description, dueDate, priority, project) {
+  let todo = new Todo(title, description, dueDate, priority, project);
+  projectsArray.forEach((item) => {
+    if (item.title == project) {
+      item.storage.push(todo);
+    }
+  });
 }
