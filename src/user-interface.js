b172c37a6591d7ac7837168afefe780814f0d79c
osp-d <edudamirosp@gmail.com>
2024-05-01 22:45:45 +0500

    Add project switching and refactor
    Created functionality that utilizes global currentProject variable to
    indicate current active project tab. Utilized showTodo to get todo array
    of the active project and to render each of the array todo objects.
    Renamed variables and reorganized DOM manipulations to increase
    readability and consistency.

--- src/user-interface.js
+++ src/user-interface.js
@@ -4,88 +4,151 @@ import {
   deleteProject,
   addTodo,
   showTodo,
+  changeTodoStatus,
   projectsArray,
 } from './todos.js';
 
-const sidebar = document.querySelector('div.sidebar');
-const main = document.querySelector('div.main');
-const addProjectBtn = document.querySelector('button.add-project');
-const form = document.querySelector('form.todo-form');
-const cancelFormBtn = document.querySelector('input.cancel');
-const menuIcon = document.querySelector('svg.menu');
+import { Todo } from './todos.js';
+import { Project } from './todos.js';
+
+// Creating DOM elements
 
 let currentProject;
 
+const sidebar = document.querySelector('div.sidebar');
+// const main = document.querySelector('div.main');
+const addProjectBtn = document.querySelector('button.add-project');
+const todoForm = document.querySelector('form.todo-form');
+const cancelFormBtn = document.querySelector('input.cancel');
+const projectMenuIcon = document.querySelector('svg.menu');
+const newProjectWrapper = document.createElement('div');
+const inputProject = document.createElement('input');
+const submitProjectBtn = document.createElement('button');
+const cancelProjectBtn = document.createElement('button');
+const addTodoBtn = document.createElement('button');
+const plusIcon = document.querySelector('svg.plus');
+const plusIconCopy = plusIcon.cloneNode(true);
+const todoMainSpace = document.querySelector('div.content');
+
 addHover(addProjectBtn, '#f3f4f6', 'transparent');
 
-const inputWrapper = document.createElement('div');
-const inputProject = document.createElement('input');
-const addBtn = document.createElement('button');
-const cancelBtn = document.createElement('button');
-
-addBtn.textContent = 'Add';
-cancelBtn.textContent = 'Cancel';
+submitProjectBtn.textContent = 'Add';
+cancelProjectBtn.textContent = 'Cancel';
 inputProject.setAttribute('placeholder', 'Enter project name');
-inputWrapper.classList.add('input-wrapper');
-addBtn.classList.add('add');
-cancelBtn.classList.add('cancel');
+newProjectWrapper.classList.add('new-project-wrapper');
+submitProjectBtn.classList.add('submit-project');
+cancelProjectBtn.classList.add('cancel-project');
+addTodoBtn.textContent = 'Add Task';
+addTodoBtn.classList.add('add-todo');
 
-inputWrapper.appendChild(inputProject);
-inputWrapper.appendChild(addBtn);
-inputWrapper.appendChild(cancelBtn);
+newProjectWrapper.appendChild(inputProject);
+newProjectWrapper.appendChild(submitProjectBtn);
+newProjectWrapper.appendChild(cancelProjectBtn);
 
 addProjectBtn.addEventListener('click', () => {
-  sidebar.append(inputWrapper);
+  sidebar.append(newProjectWrapper);
 });
 
-addBtn.addEventListener('click', () => {
+submitProjectBtn.addEventListener('click', () => {
   const projectName = document.querySelector('input').value;
   addProject(projectName);
 
-  let menuIconCopy = menuIcon.cloneNode(true);
-
+  let projectMenuIconCopy = projectMenuIcon.cloneNode(true);
   const projectBtn = document.createElement('button');
   projectBtn.classList.add('project');
   projectBtn.textContent = projectName;
-  projectBtn.prepend(menuIconCopy);
-  menuIconCopy.classList.remove('invisible');
+  projectBtn.prepend(projectMenuIconCopy);
+  projectMenuIconCopy.classList.remove('invisible');
 
   addHover(projectBtn, '#f3f4f6', 'transparent');
 
   projectBtn.addEventListener('click', () => {
+    while (todoMainSpace.firstChild) {
+      todoMainSpace.removeChild(todoMainSpace.lastChild);
+    }
+
     currentProject = projectName;
     projectBtn.style.backgroundColor = '#7dd3fc';
 
-    console.log(currentProject);
+    let projectStorage = showTodo(currentProject);
+    projectStorage.forEach((element) => {
+      const todoWrapper = document.createElement('div');
+      todoWrapper.classList.add('todo-wrapper');
+      const checkTodo = document.createElement('input');
+      checkTodo.setAttribute('type', 'checkbox');
+      const todoName = document.createElement('p');
+      todoName.textContent = element.title;
+      const todoDate = document.createElement('p');
+      todoDate.textContent = element.dueDate;
+      todoWrapper.appendChild(checkTodo);
+      todoWrapper.appendChild(todoName);
+      todoWrapper.appendChild(todoDate);
+      todoMainSpace.prepend(todoWrapper);
+    });
+
+    renderAddTodo();
   });
 
-  renderAddTodo();
-
   inputProject.value = '';
-  sidebar.removeChild(inputWrapper);
+  sidebar.removeChild(newProjectWrapper);
   sidebar.prepend(projectBtn);
 
   console.log(projectsArray);
 });
 
-cancelBtn.addEventListener('click', () => {
-  sidebar.removeChild(inputWrapper);
+cancelProjectBtn.addEventListener('click', () => {
+  sidebar.removeChild(newProjectWrapper);
 });
 
-const addTodoBtn = document.createElement('button');
-addTodoBtn.textContent = 'Add Task';
-addTodoBtn.classList.add('add-todo');
-const icon = document.querySelector('svg.plus');
-const iconCopy = icon.cloneNode(true);
+todoForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (todoForm.checkValidity() === false) {
+    todoForm.reportValidity();
+    return;
+  } else {
+    const title = document.querySelector('#title').value;
+    const description = document.querySelector('#description').value;
+    const priority = document.querySelector('select').value;
+    const date = document.querySelector('#date').value;
+
+    let element = addTodo(title, description, date, priority, currentProject);
+
+    const todoWrapper = document.createElement('div');
+    todoWrapper.classList.add('todo-wrapper');
+    const checkTodo = document.createElement('input');
+    checkTodo.setAttribute('type', 'checkbox');
+    const todoName = document.createElement('p');
+    todoName.textContent = element.title;
+    const todoDate = document.createElement('p');
+    todoDate.textContent = element.dueDate;
+    todoWrapper.appendChild(checkTodo);
+    todoWrapper.appendChild(todoName);
+    todoWrapper.appendChild(todoDate);
+    todoMainSpace.prepend(todoWrapper);
+
+    // projectStorage.forEach((element) => {});
+
+    todoForm.reset();
+    todoForm.classList.add('invisible');
+    addTodoBtn.classList.remove('invisible');
+  }
+});
+
+cancelFormBtn.addEventListener('click', () => {
+  todoForm.reset();
+  addTodoBtn.classList.remove('invisible');
+  todoForm.classList.add('invisible');
+});
 
 function renderAddTodo() {
-  addTodoBtn.prepend(iconCopy);
-  main.appendChild(addTodoBtn);
+  addTodoBtn.prepend(plusIconCopy);
+  todoMainSpace.appendChild(addTodoBtn);
 
   addHover(addTodoBtn, '#f3f4f6', 'transparent');
 
   addTodoBtn.addEventListener('click', () => {
-    form.classList.remove('invisible');
+    todoForm.classList.remove('invisible');
     addTodoBtn.classList.add('invisible');
   });
 }
@@ -99,29 +162,3 @@ function addHover(value, color1, color2) {
     value.style.backgroundColor = color2;
   });
 }
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  if (form.checkValidity() === false) {
-    form.reportValidity();
-    return;
-  } else {
-    const title = document.querySelector('#title').value;
-    const description = document.querySelector('#description').value;
-    const date = document.querySelector('#date').value;
-
-    addTodo(title, description, date, 'medium', currentProject);
-    showTodo(currentProject);
-
-    form.reset();
-    form.classList.add('invisible');
-    addTodoBtn.classList.remove('invisible');
-  }
-});
-
-cancelFormBtn.addEventListener('click', () => {
-  form.reset();
-  addTodoBtn.classList.remove('invisible');
-  form.classList.add('invisible');
-});
